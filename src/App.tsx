b494adcd1cec89c26e65246f03062d78f77fb390
gesterloricz <mimosaver68@gmail.com>
2026-01-22 01:00:09 +0800

    name of files changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
+import Header from './components/header'
 import Footer from './components/footer'
 import Home from './pages/home'
 import Demo from './pages/demo'
